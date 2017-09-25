c5472db36594939a329d640830fbe471077e4979
Michael Posso <mposso@docero.com>
2017-09-25 14:43:33 -0400

    first commit

---
+++ day1/complete/js/custom.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('.responsive-buttons a').click(function() {
+		$('.responsive-sidebar-hide').toggle();
+		
+		var text = $(this).text();
+    		
+		$(this).text(
+       		 	text == "Show sidebar" ? "Hide sidebar" : "Show sidebar");
+	});
+
+	
+});
